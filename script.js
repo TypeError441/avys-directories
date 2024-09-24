0cb1525204ba28a82b5046a094e32e71b3095626
TypeError441 <78857657+TypeError441@users.noreply.github.com>
2024-09-23 19:37:27 -0700

    bv

--- script.js
+++ script.js
@@ -1,16 +1,8 @@
 import { initializeApp } from 'https://www.gstatic.com/firebasejs/10.13.2/firebase-app.js'
 import { getAuth, createUserWithEmailAndPassword } from 'https://www.gstatic.com/firebasejs/10.13.2/firebase-auth.js'
+import { firebaseConfig } from './utils/config';
 
 // Initialize Firebase
-const firebaseConfig = {
-    apiKey: "AIzaSyATqdN5VIW0QvXl3jQQ1ev9mrCEdr-edhM",
-    authDomain: "avys-directories.firebaseapp.com",
-    projectId: "avys-directories",
-    storageBucket: "avys-directories.appspot.com",
-    messagingSenderId: "164736308154",
-    appId: "1:164736308154:web:fed9592f94094a072d4383",
-    measurementId: "G-B38541VHW0"
-};
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 
---
+++ utils/config.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyATqdN5VIW0QvXl3jQQ1ev9mrCEdr-edhM",
+    authDomain: "avys-directories.firebaseapp.com",
+    projectId: "avys-directories",
+    storageBucket: "avys-directories.appspot.com",
+    messagingSenderId: "164736308154",
+    appId: "1:164736308154:web:fed9592f94094a072d4383",
+    measurementId: "G-B38541VHW0"
+};
