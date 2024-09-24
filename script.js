861b7befad0b8b360b5be3c48b4f5db4b1e5b07e
TypeError441 <78857657+TypeError441@users.noreply.github.com>
2024-09-23 21:41:21 -0700

    a

--- script.js
+++ script.js
@@ -8,6 +8,16 @@ const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 
 $(document).ready(function() {
+    if(localStorage.getItem("email") != null && localStorage.getItem("password") != null) {
+        signInWithEmailAndPassword(auth, localStorage.getItem("email"), localStorage.getItem("password"))
+        .then((userCredential) => {
+            const user = userCredential.user;
+            $(".login").hide();
+            $(".register").hide();
+
+            $(".directories").show();
+        })
+    } else {
     $(".directories").hide();
     $(".login").show();
     $(".login > .errorText").hide();
@@ -20,6 +30,8 @@ $(document).ready(function() {
             const user = userCredential.user;
             $(".login").hide();
             $(".register").hide();
+            localStorage.setItem("email", $(".login > input[type='email']").val());
+            localStorage.setItem("password", $(".login > input[type='password']").val());
 
             $(".directories").show();
         })
@@ -37,6 +49,8 @@ $(document).ready(function() {
             const user = userCredential.user;
             $(".login").hide();
             $(".register").hide();
+            localStorage.setItem("email", $(".login > input[type='email']").val());
+            localStorage.setItem("password", $(".login > input[type='password']").val());
 
             $(".directories").show();
         })
@@ -47,23 +61,20 @@ $(document).ready(function() {
             $(".register > .errorText").show();
         });
     });
+    }
+
+    $(".exit").click(function() {
+        signOut(auth).then(() => {});
+        
+        localStorage.setItem("email", null);
+        localStorage.setItem("password", null);
+
+        $(".directories").hide();
+        $(".login").show();
+        $(".login > .errorText").hide();
+        $(".register").hide();
+        $(".register > .errorText").hide();
+    });
 });
 
 // auth.currentUser
-// createUserWithEmailAndPassword(auth, email, password)
-// .then((userCredential) => {
-//     const user = userCredential.user;
-// })
-// .catch((error) => {
-//     const errorCode = error.code;
-//     const errorMessage = error.message;
-// });
-
-// signInWithEmailAndPassword(auth, email, password)
-// .then((userCredential) => {
-//     const user = userCredential.user;
-// })
-// .catch((error) => {
-//     const errorCode = error.code;
-//     const errorMessage = error.message;
-// });
